9ec5f498271b67c41ceff6f4ccc57db94bd2b575
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-08-21 20:58:37 -0300

    Api Tipos

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TipoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/tipos/',[TipoController::class,'index'])->name('tipos.index');
+
+
+//Route::resources('/tipos',TipoController::class);
